d03c487db38f3cf6e8481296d6bcd2729bff385d
Styrs <105436183+Styrs@users.noreply.github.com>
2025-10-30 17:41:27 +0100

    Add files via upload

---
+++ data_transform_2025.R
@@ -0,0 +1,44 @@
+
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(zoo)
+library(writexl)
+
+##---------------------------------------------------------------------
+## We import the data
+##---------------------------------------------------------------------
+
+url_euro_countries <- "https://github.com/Styrs/Forcasting_euro_growth/raw/refs/heads/main/Countries_Excel_euro_GDP.xlsx"
+
+download.file(url_euro_countries, destfile = "data.xlsx", mode = "wb")
+
+data_countries_euro <- read_excel("data.xlsx")
+
+head(data_countries_euro)
+
+
+##---------------------------------------------------------------------
+## Organizing the data
+##---------------------------------------------------------------------
+
+data_countries_euro_prepared <- data_countries_euro %>%
+  rename(Country = TIME) %>%            # rename first column
+  pivot_longer(
+    cols = -Country,                    # all other columns are quarters
+    names_to = "Quarter",
+    values_to = "Nominal_GDP"
+  )
+
+
+
+data_countries_euro_prepared <- data_countries_euro_prepared %>%
+  mutate(Quarter = as.yearqtr(Quarter, format = "%Y-Q%q"))
+
+
+
+
+
+
+
+
